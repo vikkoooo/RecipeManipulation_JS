f6ea941c4004c5f9f832aaf176eaaacb94686f17
vikkoooo <vikkolundberg@gmail.com>
2024-07-05 13:00:05 +0200

    extract image object, ingredients, instructions

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ console.log(description.style.fontSize); // to log after manually set
 
 // What is the value of the alt atrribute on the image?
 const image = document.querySelector(".image-container img");
-const imageAlt = image.getAttribute("alt");
+const imageAlt = image.getAttribute("alt"); // Answer: Slice of Frozen Cheescake
 console.log(imageAlt);
 
 // What is the dimensions and the url of the image? Create an object that looks like this, and log it to the console:
@@ -25,13 +25,50 @@ console.log(imageAlt);
 //  	height: number,
 //  	width: number,
 //	}
+const imageObject = {
+	url: image.src,			// Answer: url "http://127.0.0.1:5500/assets/frozen-cheesecake-slice.jpg"
+	height: image.height,	// Answer: height: 675
+	width: image.width		// Answer: width: 811
+}
+console.log(imageObject);
 
+// How many ingredients has the paste?
+const ingredients = document.querySelector("ul.ingredients-list-paste");
+const ingredientsLength = ingredients.children.length; // Answer: 5
+console.log(ingredientsLength);
 
+// Which is the forth element in the list containing the ingredients for the paste?
+const ingredientsForth = ingredients.children[3].innerText; // 3dl vispgrädde
+console.log(ingredientsForth);
 
-//How many ingredients has the paste?
-//Which is the forth element in the list containing the ingredients for the paste?
-//Create an an array of objects from the instructions. Each element in the array should be an object that looks like this:
-//{
-//  order: number;
-//  text: instruction;
-//}
+// Create an an array of objects from the instructions. Each element in the array should be an object that looks like this:
+// 	{
+//  	order: number;
+//  	text: instruction;
+//	}
+const instructions = document.querySelectorAll("ol.instructions-list li"); // get each li item (returns NodeList)
+console.log(instructions); // before
+
+const instructionsArray = [];
+
+for (let i = 0; i < instructions.length; i++) {
+	const outputObject = {
+		order: i + 1,
+		text: instructions[i].innerText
+	};
+	instructionsArray.push(outputObject);
+}
+
+console.log(instructionsArray); // after
+/* Answers:
+{order: 1, text: 'Smula sönder 10 stycken digistivetex och blanda se… ska bli som en tjockt täcke på botten av formen.'}
+{order: 2, text: 'Separera ägggulor och äggvitor. Äggvitorna lägger du i en stor bunke, äggulorna i en liten bunke.'}
+{order: 3, text: 'Vispa äggvitorna hårt med en elvisp. De är tillräc…n vända uppochner på bunken utan att det rör sig.'}
+{order: 4, text: 'Häll upp grädden i en mellanstor bunke. Vispa de relativ hårt. En aning hårdare en vanligt.'}
+{order: 5, text: 'Blanda i philadelphiaosten och vaniljsockret med äggulorna och vispa ihop.'}
+{order: 6, text: 'Häll nu över innehållet i bunken med äggulor över grädden. Vispa ihop på lägsta hastigheten.'}
+{order: 7, text: 'Häll sedan över det innehållet till bunken med ägg…ed en gaffel tills det blir en slät fluffig smet.'}
+{order: 8, text: 'Häll smeten i formen, smula över de resterande digistivetexen om du vill.'}
+{order: 9, text: 'Ställ in i frysen över natten.'}
+{order: 10, text: 'Servera och toppa med det du känner för.'}
+*/
